d7bd845f3ced0f8615205ecb404f515ba5870675
Gokay Arpaci <gokayarpaci@gmail.com>
2019-07-15 18:38:46 +0300

    library app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,30 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { async, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
+import { AppModule } from "./app.module";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppModule, RouterTestingModule]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("render", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'LibraryApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('LibraryApp');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it("render app links", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to LibraryApp!');
-  });
+    expect(
+      compiled.querySelector("[data-test-toolbar-books-link]").textContent
+    ).toContain("Books");
+    expect(
+      compiled.querySelector("[data-test-toolbar-authors-link]").textContent
+    ).toContain("Authors");
+  }));
 });
